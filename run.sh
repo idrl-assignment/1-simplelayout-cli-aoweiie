bff4138abf605e37755eac1e4af11b90c4cc0859
WJ Yao <996267113@qq.com>
2020-11-02 23:25:16 +0800

    创建shell脚本

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python simplelayout.py \
+    --board_grid 100 \
+    --unit_grid 10 \
+    --unit_n 3 \
+    --positions 1 15 33 \
+    --outdir dir1/dir2 \
+    --file_name example \
+
